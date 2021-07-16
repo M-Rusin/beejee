f8383e24b52918b3827b16fcc29a988912f6e0df
M. S. Rusin <rusin.m.s@gmail.com>
2021-07-16 20:17:23 +0300

    Update controller_todo.php

--- application/controllers/controller_todo.php
+++ application/controllers/controller_todo.php
@@ -7,10 +7,11 @@ class Controller_Todo extends Controller
   { 
     unset($_SESSION['open_new_edit_window']);
     $currentPage = $_GET['page'] ?? 1;
-
+    $currentSort = $_GET['sort'];
+    $currentOrder = $_GET['order'];
     $model = new Model_Todo();
-    $data = $model->paginatorData(PAGINATION_COUNT , $currentPage);
-    $paginator =  $model->paginatorLinks(PAGINATION_COUNT, $currentPage);
+    $data = $model->paginatorData(PAGINATION_COUNT , $currentPage, $currentSort, $currentOrder);
+    $paginator =  $model->paginatorLinks(PAGINATION_COUNT, $currentPage, $currentSort, $currentOrder);
 
     $this->view->generate('index_todo_view.php', 'template_view.php', $data, $paginator);
   }
@@ -25,19 +26,19 @@ class Controller_Todo extends Controller
   { 
     unset($_SESSION['open_new_edit_window']);
   	$model = new Model_Todo();
-    $result = $model->insert();
+    $result = $model->insert($_POST['name'], $_POST['email'], $_POST['description']);
 
     if($result)
-      $msg = "Успешно сохранено";
+      $_SESSION['message'] = "Успешно сохранено";
     else 
-      $msg = "Ошибка сохранения";
+      $_SESSION['message'] = "Ошибка сохранения";
 
-    $this->view->generate('404_view.php', 'template_view.php', $msg);
+    header('Location:/todo/');
   }
 
   function action_edit()
-  { 
-    if(!checkAuth()) header('Location:/todo/');
+  {     
+    $this->relog();
     if(isset($_SESSION['open_new_edit_window'])){
       header('Location:/login/logout');
     } else {
@@ -45,24 +46,32 @@ class Controller_Todo extends Controller
     }
 
     $model = new Model_Todo();
-    $data = $model->getById();
+    $data = $model->getById($_GET['id']);
     
     $this->view->generate('edit_todo_view.php', 'template_view.php', $data);
   }
 
   function action_update()
   { 
-    if(!checkAuth()) header('Location:/todo/');
+    $this->relog();
     unset($_SESSION['open_new_edit_window']);
 
     $model = new Model_Todo();
-    $result = $model->update();
+    if(!$_POST['status']) $_POST['status'] = 1;
+    $result = $model->update($_GET['id'], $_POST['name'], $_POST['email'], $_POST['description'], $_POST['status']);
 
     if($result)
-      $msg = "Успешно сохранено";
+      $_SESSION["message"] = "Успешно сохранено";
     else 
-      $msg = "Ошибка сохранения";
+      $_SESSION["message"] = "Ошибка сохранения";
 
-    $this->view->generate('404_view.php', 'template_view.php', $msg);
+    header('Location:/todo/');
+  }
+
+  function relog(){
+    if($_SESSION['login_status'] !== 'access_granted'){
+      $_SESSION["message"] = "Пройдите авторизацию";
+      exit(header('Location:/login/'));
+    }
   }
 }
