a4d18d78c7b9532dcb7a37674f726dcce345435d
M. S. Rusin <rusin.m.s@gmail.com>
2021-07-16 20:16:55 +0300

    Update model_todo.php

--- application/models/model_todo.php
+++ application/models/model_todo.php
@@ -3,7 +3,6 @@
 class Model_Todo extends Model
 {
 	protected $valid;
-	const STATUS = 1;
 	
 	public function __construct()
 	{
@@ -12,13 +11,15 @@ class Model_Todo extends Model
 		$this->valid = new Validete();
 	}
 
-	public function paginatorData($count,int $from)
+	public function paginatorData($count,int $from, $sort, $order)
 	{
-		$data = []; //, `name` DESC
-		$query = "SELECT * FROM `todos` ORDER BY `id` ";
+		$data = [];
+		$validInjection = $this->valid->validInject($sort, $order);
+		extract($validInjection);
+		$query = "SELECT * FROM `todos` ORDER BY $sort $order ";
 		$limit = "LIMIT $count";
 		
-		if ( $from > 1)
+		if ( $from > 0)
 		{
 			$from = --$from*$count;
 			$limit = "LIMIT $from, $count";
@@ -38,30 +39,26 @@ class Model_Todo extends Model
 				];
 			}
 		}
-
 		return $data;
 	}
 	
-	public function insert()
+	public function insert($name, $email, $text)
 	{
-		$name 	= $_POST['name']; 
-		$email 	= $_POST['email']; 
-		$text 	= $_POST['description'];
 		
-		$name 	= $this->valid->validName($name); 
-		$email 	= $this->valid->validEmail($email); 
-		$text 	= $this->valid->validText($text); 
+		$name 	= $this->valid->validFields($name); 
+		$email 	= $this->valid->validFields($email); 
+		$text 	= $this->valid->validFields($text); 
+		$created_at = date('Y-m-d H:i:s');
 		
-		$query = "INSERT INTO `todos`(`name`, `email`, `text`) VALUES ('$name', '$email', '$text')";
+		$query = "INSERT INTO `todos`(`name`, `email`, `text`, `created_at`) VALUES ('$name', '$email', '$text', '$created_at')";
 		$result = $this->query($query) ?? '';
 
 		return $result;
 	}
 
-	public function getById()
+	public function getById($id)
 	{
-		$id = $_GET['id'];
-		$id = $this->valid->validId($id);
+		$id = $this->valid->validFields($id);
 
 		$query = "SELECT * FROM `todos` WHERE id=$id";
 		$result = $this->query($query) ?? '';
@@ -70,21 +67,17 @@ class Model_Todo extends Model
 		return $row;
 	}
 
-	public function update()
+	public function update(int $id, $name, $email, $description, int $status=1)
 	{
-		$id = $_GET['id'];
-		$name 	= $_POST['name']; 
-		$email 	= $_POST['email']; 
-		$text 	= $_POST['description'];
-		$status = static::STATUS;
 		$updated_at = date('Y-m-d H:i:s');
 
-		$id 	= $this->valid->validId($id);
-		$name 	= $this->valid->validName($name); 
-		$email 	= $this->valid->validEmail($email); 
-		$text 	= $this->valid->validText($text); 
-		
-		$query = "UPDATE `todos` SET `name`='$name',`email`='$email',`text`='$text',`status`=$status,`updated_at`='$updated_at' WHERE id=$id";
+		$id 	= $this->valid->validFields($id);
+		$name 	= $this->valid->validFields($name); 
+		$email 	= $this->valid->validFields($email); 
+		$text 	= $this->valid->validFields($description); 
+
+		$query = "UPDATE `todos` SET `name`='$name',`email`='$email',`text`='$text',`status`= if(`status` < 2, $status, `status`),`updated_at`='$updated_at' WHERE id=$id";
+
 		$result = $this->query($query) ?? '';
 
 		return $result;
@@ -99,13 +92,16 @@ class Model_Todo extends Model
 		return $row[0]; 
 	}
 
-	public function paginatorLinks($limit, $currentPage)
+	public function paginatorLinks($limit, $currentPage, $sort, $order)
 	{
+
+		$validInjection = $this->valid->validInject($sort, $order);
+		extract($validInjection);
+
 		$total =  $this->getCount('todos');
+		$total++;
 		if ($total <= $limit) return false;
-
-		$paginator = new Pagination($total, $currentPage, $limit, '?page=');
-
+		$paginator = new Pagination($total, $currentPage, $limit, $index='page', $sort, $order);
 		return $paginator->links();
 	}
 
@@ -115,27 +111,20 @@ class Model_Todo extends Model
 
 class Validete
 {
-	public function validName($var)
-	{
-		// можно тут сколько угодно валидировать
-		return trim($var);
-	}
-	
-	public function validEmail($var)
-	{
-		// можно тут сколько угодно валидировать
-		return trim($var);
-	}
-	public function validText($var)
+	public function validFields($var)
 	{
 		// можно тут сколько угодно валидировать 
-		return htmlspecialchars(trim($var));
-	}
-
-	public function validId($var)
-	{
-		// можно тут сколько угодно валидировать 
-		return $var;
+		return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
 	}
 	
+	public function validInject($sort, $order)
+	{
+		//захардкодил валидацию сортировки в массивах от инъекций
+		$validSorts = array('id', 'name', 'email', 'status');
+		$validOrders = array('ASC', 'DESC');
+		$arr = array();
+		in_array($sort, $validSorts) ? $arr['sort'] = $sort : $arr['sort'] = 'id';
+		in_array($order, $validOrders) ? $arr['order'] = $order : $arr['order'] = 'ASC';
+		return $arr;
+	}
 }
